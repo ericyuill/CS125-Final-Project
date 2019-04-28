0c7662ca29c39f9a5c7a74beab17c920d8410c59
Eric Yuill <ericy2@illinois.edu>
2019-04-27 19:52:24 -0500

    timer01

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     private Button mButtonReset;
     private CountDownTimer mCountDownTimer;
     private boolean mTimerRunning;
-    private long mTimeLeftInMIllis = START_TIME_IN_MILLIS;
+    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
 
     //variables for counting the keeping score
     public static final String final_Score = "com.example.myapplication.final_Score";
@@ -37,9 +37,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        /**
-        mTextViewCountDown = findViewById(R.id.text_view_countdown);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        scorer = findViewById(R.id.score);
+        question = findViewById(R.id.question);
+        falseButton = findViewById(R.id.False);
+
+        mTextViewCountDown = findViewById(R.id.text_view_countdown);
         mButtonStartPause = findViewById(R.id.button_start_pause);
         mButtonReset = findViewById(R.id.button_reset);
 
@@ -61,81 +66,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        updateCountDownText();
+
         mTimerRunning = true;
         mButtonStartPause.setText("pause");
         mButtonReset.setVisibility(View.INVISIBLE);
-
-         **/
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_main);
-
-        Button trueButton = findViewById(R.id.True);
-        trueButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onStart();
-            }
-        });
-
-        scorer = findViewById(R.id.score);
-        question = findViewById(R.id.question);
-
-        trueButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                updateScore(keepScore);
-            }
-        });
-
-        falseButton = findViewById(R.id.False);
-        falseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SharedPreferences preferences = getSharedPreferences("PREFS", 0);
-                SharedPreferences.Editor editor = preferences.edit();
-                editor.putInt("lastScore", keepScore);
-                editor.apply();
-                gameOver();
-            }
-        });
     }
 
-    private void updateScore(int currentScore) {
-        keepScore++;
-        scorer.setText("" + keepScore);
-    }
-    private void gameOver() {
-        int endScore = Integer.parseInt(scorer.getText().toString());
-        Intent intent = new Intent(this, GameOver.class);
-        intent.putExtra(final_Score, endScore);
-        startActivity(intent);
-    }
-
-    private void pauseTimer() {
-        mCountDownTimer.cancel();
-        mTimerRunning = false;
-        mButtonStartPause.setText("start");
-        mButtonReset.setVisibility(View.VISIBLE);
-    }
-    private void resetTimer() {
-        mTimeLeftInMIllis = START_TIME_IN_MILLIS;
-        updateCountDownText();
-        mButtonReset.setVisibility(View.INVISIBLE);
-        mButtonStartPause.setVisibility(View.VISIBLE);
-
-    }
-    private void updateCountDownText() {
-        int minutes = (int) (mTimeLeftInMIllis / 1000) / 60;
-        int seconds = (int) (mTimeLeftInMIllis / 1000) % 60;
-        String timeLeftFormatted = String.format(Locale.getDefault(),"%02d:%02d", minutes, seconds);
-        mTextViewCountDown.setText(timeLeftFormatted);
-    }
     private void startTimer() {
-        mCountDownTimer = new CountDownTimer(mTimeLeftInMIllis, 1000) {
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
-                mTimeLeftInMIllis = millisUntilFinished;
+                mTimeLeftInMillis = millisUntilFinished;
                 updateCountDownText();
             }
 
@@ -148,5 +90,66 @@ public class MainActivity extends AppCompatActivity {
 
             }
         }.start();
+
+        mTimerRunning = true;
+        mButtonStartPause.setText("pause");
+        mButtonReset.setVisibility(View.INVISIBLE);
+    }
+
+    private void pauseTimer() {
+        mCountDownTimer.cancel();
+        mTimerRunning = false;
+        mButtonStartPause.setText("start");
+        mButtonReset.setVisibility(View.VISIBLE);
+    }
+
+    private void resetTimer() {
+        mTimeLeftInMillis = START_TIME_IN_MILLIS;
+        updateCountDownText();
+        mButtonReset.setVisibility(View.INVISIBLE);
+        mButtonStartPause.setVisibility(View.VISIBLE);
+    }
+
+    private void updateCountDownText() {
+        int minutes = (int) (mTimeLeftInMillis / 1000) / 60;
+        int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
+        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+        mTextViewCountDown.setText(timeLeftFormatted);
+    }
+    Button trueButton = findViewById(R.id.True);
+        trueButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            onStart();
+        }
+    });
+        trueButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            updateScore(keepScore);
+        }
+    });
+        falseButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            SharedPreferences preferences = getSharedPreferences("PREFS", 0);
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putInt("lastScore", keepScore);
+            editor.apply();
+            gameOver();
+        }
+    });
+}
+
+    private void updateScore(int currentScore) {
+        keepScore++;
+        scorer.setText("" + keepScore);
+    }
+
+    private void gameOver() {
+        int endScore = Integer.parseInt(scorer.getText().toString());
+        Intent intent = new Intent(this, GameOver.class);
+        intent.putExtra(final_Score, endScore);
+        startActivity(intent);
     }
 }
